0eeabe9707a147a98ab98ceceb8474ed65f42fe5
Zmago Devetak <zmago_devetak@yahoo.com>
2015-09-30 07:43:21 +0000

    added validations to organisation model

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,4 +3,22 @@ class Organisation < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+         
+  validates :name, presence: true , length: { maximum: 140 }
+  validates :address, presence: true , length: { maximum: 140 }
+  validates :zip, presence: true 
+  validates :city, presence: true, length: { maximum: 140 } 
+  validates :phone, presence: true, length: { maximum: 140 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+                    
+  validates :website, length: { maximum: 140 } 
+  validates :description, length: { maximum: 140 }
+  
+  validates :password, presence: true, length: { minimum: 8 } 
+  validates :password_confirmation, presence: true, length: { minimum: 8 }       
+  
 end
